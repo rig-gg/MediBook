74a2928d1efa1e7afdad922f7ca54c62c9fd200d
rig-gg <gyle.amihan@cit.edu>
2026-07-07 13:27:07 +0800

    feat(web): add doctorService for fetching doctor directory from API

---
+++ web/src/services/doctorService.js
@@ -0,0 +1,13 @@
+import axiosInstance from '../api/axiosInstance';
+
+// Fetch all doctors, optionally filtered by specialization
+export const getDoctors = async (specialization = '') => {
+  const params = specialization ? { specialization } : {};
+  const response = await axiosInstance.get('/doctors', { params });
+  return response.data;
+};
+
+export const getDoctorById = async (doctorId) => {
+  const response = await axiosInstance.get(`/doctors/${doctorId}`);
+  return response.data;
+};
