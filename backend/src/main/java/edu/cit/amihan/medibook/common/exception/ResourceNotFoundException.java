454d6a34842335ecf8699f495998bcd5007a5d17
rig-gg <gyle.amihan@cit.edu>
2026-07-07 13:25:19 +0800

    feat(backend): add ResourceNotFoundException and GlobalExceptionHandler for common error handling

---
+++ backend/src/main/java/edu/cit/amihan/medibook/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package edu.cit.amihan.medibook.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
