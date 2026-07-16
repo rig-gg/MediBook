294647f4e2ffa7ba120467be379c975a6bf83a60
rig-gg <gyle.amihan@cit.edu>
2026-07-16 19:04:38 +0800

    chore: migrate deployment from Railway to Render

--- mobile/app/src/main/java/edu/cit/amihan/medibook/core/network/RetrofitClient.kt
+++ mobile/app/src/main/java/edu/cit/amihan/medibook/core/network/RetrofitClient.kt
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "https://medibook-api.onrender.com/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = if (android.util.Log.isLoggable("Retrofit", android.util.Log.DEBUG))
